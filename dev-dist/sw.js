503ad211d2ee449932e34802360b0be56bbe1165
devd-328 <devd66418@gmail.com>
2026-04-09 14:56:27 +0500

    chore: sync generated sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.c2lbrv6o10g"
+    "revision": "0.ajkfehqb9rc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
